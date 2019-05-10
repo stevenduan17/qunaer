bfba66481bb8ec788174a73c7083aa54ca0e16ff
ddx <stevenduan17@gmail.com>
2019-05-10 15:22:27 +0800

    add detail header.

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     <banner></banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
   import Banner from '@/components/detail/Banner'
+  import DetailHeader from '@/components/detail/Header'
+
   export default {
     name: 'Detail',
-    components: { Banner }
+    components: { Banner, DetailHeader }
   }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+
+  .content
+    height 50rem
 
 </style>
---
+++ src/components/detail/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <router-link
+      v-show="showIcon"
+      to="/"
+      tag="div"
+      class="header-abs">
+      <span class="iconfont">&#xe624;</span>
+    </router-link>
+    <div class="header-fixed" v-show="!showIcon" :style="opacityStyle">
+      景点详情
+      <router-link to="/">
+        <div class="back">
+          <span class="iconfont">&#xe624;</span>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data () {
+      return {
+        showIcon: true,
+        opacityStyle: {
+          opacity: 0
+        }
+      }
+    },
+    activated () {
+      window.addEventListener('scroll', () => {
+        const top = document.documentElement.scrollTop
+        if (top > 60) {
+          let opacity = top / 140
+          opacity = opacity > 1 ? 1 : opacity
+          this.opacityStyle = { opacity }
+          this.showIcon = false
+        } else {
+          this.showIcon = true
+        }
+      })
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles"
+
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    border-radius .4rem
+    background: rgba(0, 0, 0, .8)
+    text-align center
+
+    span
+      color white
+      line-height .8rem
+
+  .header-fixed
+    position fixed
+    overflow hidden
+    top 0
+    left 0
+    right 0
+    height .86rem
+    line-height .86rem
+    font-size .32rem
+    text-align center
+    color white
+    background $bg-color
+
+    .back
+      color white
+      position absolute
+      font-size .4rem
+      width .64rem
+      text-align center
+      top 0
+      left 0
+</style>
